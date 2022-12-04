12dccbd79e02e22453aed7099bf32de5295cfe9b
vineetkhemnani <vineetkhemnani09@gmail.com>
2022-12-04 13:53:23 +0530

    solved an error

--- commands/organize.js
+++ commands/organize.js
@@ -1,5 +1,26 @@
 const fs = require("fs");
 const path=require("path");
+let types = {
+    media: ["mp4", "mkv", "mp3", "jpg", "png", "jpeg", "gif"],
+    archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+    documents: [
+      "docx",
+      "doc",
+      "pdf",
+      "xlsx",
+      "xls",
+      "odt",
+      "ods",
+      "odp",
+      "odg",
+      "odf",
+      "txt",
+      "ps",
+      "tex",
+      "pptx"
+    ],
+    app: ["exe", "dmg", "pkg", "deb"],
+  };
 function organizeFn(dirPath){
     let destPath;
     console.log("Organize command implemented for ", dirPath);
